03a91db54b9399d21825175663867c816289b9f3
ravenusmc <mcuddy77@gmail.com>
2018-08-10 15:58:17 -0400

    added base html file and connected signup to login page

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ from flask import Flask, session, jsonify, redirect, url_for, escape, render_tem
 #Setting up Flask
 app = Flask(__name__)
 
-
 #This route takes the user to the home page
 @app.route('/')
 def landing():
     return render_template('index.html')
 
-
+#This route takes the user to the sign up page 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 #This line will actually run the app.
 if __name__ == '__main__':
